897ca587e911576226dd466464f40001164c5e87
Aman Pal <palaman2626@gmail.com>
2026-02-04 12:07:52 +0530

    deployBug: attentionWithClient Updated

--- services/attention/attention.service.ts
+++ services/attention/attention.service.ts
@@ -1,57 +1,37 @@
 import prisma from "@/lib/prisma"
-import { startOfDay,endOfDay } from "@/utils/dateTime"
-import { CreateAttentionInput } from "@/types/attention"
-import { clientService } from "../client/client.service"
+import { startOfDay, endOfDay } from "@/utils/dateTime"
+import { Prisma } from "@prisma/client"
 
-class AttentionService{
-    async getTodaysAttentions(userId: string) {
-        const todayStart = startOfDay()
-        const todayEnd = endOfDay()
-        
-        return prisma.attention.findMany({
-            where: {
-                userId,
-                status: "OPEN",
-                dueDate: {
-                    gte: todayStart,
-                    lte: todayEnd,
-                },
-            },
-            orderBy: [
-                { priority: "desc" },
-                { dueDate: "asc" },
-            ],
-            include: {
-                client: true,
-            },
-        })
-    }
+export type AttentionWithClient =
+  Prisma.AttentionGetPayload<{
+    include: { client: true }
+  }>
 
-    async createAttention(input : CreateAttentionInput){
-        const {userId,clientId,type,title,description,amount,dueDate,priority = "MEDIUM"} = input
+class AttentionService {
+  async getTodaysAttentions(
+    userId: string
+  ): Promise<AttentionWithClient[]> {
+    const todayStart = startOfDay()
+    const todayEnd = endOfDay()
 
-        
-        //ensure ownership of client to a user
-        await clientService.assertClientOwnerhip({clientId,userId});
-
-        //create attention
-        return prisma.attention.create({
-            data:{
-                userId,
-                clientId,
-                type,
-                title,
-                description,
-                amount,
-                dueDate,
-                priority,
-                lastActionAt:new Date(),
-            }
-        })
-
-
-    }
+    return prisma.attention.findMany({
+      where: {
+        userId,
+        status: "OPEN",
+        dueDate: {
+          gte: todayStart,
+          lte: todayEnd,
+        },
+      },
+      orderBy: [
+        { priority: "desc" },
+        { dueDate: "asc" },
+      ],
+      include: {
+        client: true,
+      },
+    })
+  }
 }
 
-export const attentionService = new AttentionService();
-    
+export const attentionService = new AttentionService()
